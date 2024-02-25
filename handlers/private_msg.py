f0a1f23023fd063e372aa72844efbd1d223c19ec
Artem Oluwo <oluwoartem@gmail.com>
2024-02-25 15:07:30 +0300

    added buttons

--- handlers/private_msg.py
+++ handlers/private_msg.py
@@ -1,8 +1,10 @@
 from aiogram import Router, types, Bot, F
 from aiogram.filters import CommandStart, Command
-from aiogram.fsm.state import StatesGroup
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 
 from keyboards.inline import create_buttons_inline
+from keyboards.group_kb import group_dict
 
 private_router = Router()
 
@@ -14,24 +16,64 @@ BASE_KB = create_buttons_inline(
     sizes=(1, 1)
 )
 
+COURSE_KB = create_buttons_inline(
+    buttons={
+        '1 курс': 'course_1',
+        '2 курс': 'course_2',
+        '3 курс': 'course_3',
+        '4 курс': 'course_4',
+        'Назад': 'home'
+    },
+    sizes=(2, 2)
+)
+
 
 class BotMessage:
+    """
+    Класс для хранения первого сообщения Бота.
+
+    Attributes:
+        initial_message (types.Message): Первое сообщение пользователя.
+    """
+
     initial_message: types.Message = None
 
 
 @private_router.message(CommandStart())
-async def hello(message: types.Message):
-    BotMessage.initial_message = await message.answer(
-        'Здравствуйте! Пожалуйста нажмите на кнопку ниже👇', reply_markup=BASE_KB
-    )
+async def hello(message: types.Message, bot: Bot):
+    if BotMessage.initial_message is None:
+        BotMessage.initial_message = await message.answer(
+            'Здравствуйте! Пожалуйста нажмите на кнопку ниже👇', reply_markup=BASE_KB
+        )
+    else:
+        await bot.delete_message(BotMessage.initial_message.chat.id, message.message_id)
+        await bot.edit_message_text('Здравствуйте! Пожалуйста нажмите на кнопку ниже👇',
+                                    chat_id=BotMessage.initial_message.chat.id,
+                                    message_id=BotMessage.initial_message.message_id,
+                                    reply_markup=COURSE_KB)
+
+
+# @private_router.callback_query(F.data == 'home')
+# async def home(callback: types.CallbackQuery, bot: Bot):
 
 
 class AddUserGroup(StatesGroup):
-    pass
+    user_id = State()
+    user_course = State()
+    user_group = State()
 
 
 @private_router.callback_query(F.data == 'group_dashboard')
 async def group_dashboard(callback: types.CallbackQuery, bot: Bot):
     if BotMessage.initial_message:
-        await bot.edit_message_text('Worked', chat_id=BotMessage.initial_message.chat.id,
-                                    message_id=BotMessage.initial_message.message_id)
+        await bot.edit_message_text('Пожалуйста выберите ваш курс👇', chat_id=BotMessage.initial_message.chat.id,
+                                    message_id=BotMessage.initial_message.message_id,
+                                    reply_markup=COURSE_KB)
+
+
+@private_router.callback_query(F.data.startswith('course_'))
+async def get_course(callback: types.CallbackQuery, bot: Bot):
+    if BotMessage.initial_message:
+        await bot.edit_message_text('Пожалуйста выберите ваш группу👇', chat_id=BotMessage.initial_message.chat.id,
+                                    message_id=BotMessage.initial_message.message_id,
+                                    reply_markup=group_dict[int(callback.data.split('_')[-1])])
---
+++ keyboards/group_kb.py
@@ -0,0 +1,64 @@
+from keyboards.inline import create_buttons_inline
+
+
+FIRST_COURSE_KB = create_buttons_inline(
+    buttons={
+        '131': '131',
+        '132': '132',
+        '133': '133',
+        '134': '134',
+        '135': '135',
+        '136': '136',
+        '137': '137',
+        '31': '31',
+        '32': '32',
+        'Назад': 'group_dashboard'
+    },
+    sizes=(2, 2, 1, 1, 1, 2, 1)
+)
+
+SECOND_COURSE_KB = create_buttons_inline(
+    buttons={
+        '221': '221',
+        '222': '222',
+        '223': '223',
+        '224': '224',
+        '225': '225',
+        '226': '226',
+        '227': '227',
+        '21': '21',
+        '22': '22',
+        'Назад': 'group_dashboard'
+    },
+    sizes=(2, 2, 1, 1, 1, 2, 1)
+)
+
+THIRD_COURSE_KB = create_buttons_inline(
+    buttons={
+        '311': '311',
+        '312': '312',
+        '313': '313',
+        '314': '314',
+        '315': '315',
+        '316': '316',
+        '11': '11',
+        '12': '12',
+        'Назад': 'group_dashboard'
+    },
+    sizes=(2, 2, 1, 1, 2, 1)
+)
+
+FOURTH_COURSE_KB = create_buttons_inline(
+    buttons={
+        '401': '401',
+        '402': '402',
+        '403': '403',
+        '404': '404',
+        '405': '405',
+        '406': '406',
+        'Назад': 'group_dashboard'
+    },
+    sizes=(2, 2, 1, 1, 1)
+)
+
+group_dict = {1: FIRST_COURSE_KB, 2: SECOND_COURSE_KB, 3: THIRD_COURSE_KB, 4: FOURTH_COURSE_KB}

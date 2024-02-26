70dffabab44a1fd1f1336b0887832708527b0e77
Artem Oluwo <oluwoartem@gmail.com>
2024-02-27 01:18:29 +0300

    added session

--- database/database.py
+++ database/database.py
@@ -8,9 +8,9 @@ DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{D
 
 
 engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
+    async with Async_session as session:
         yield session

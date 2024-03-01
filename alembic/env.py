974ab1e9019160b6a9f853ae485af51d3a990c25
aserold <oluwoartem@gmail.com>
2024-03-01 15:45:55 +0300

    new migrations for subs

--- alembic/env.py
+++ alembic/env.py
@@ -9,7 +9,8 @@ import os
 import sys
 
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from database.models import metadata
+from database.schedule.models import schedule_metadata
+from database.subs.models import subs_metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -33,7 +34,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = [schedule_metadata, subs_metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:

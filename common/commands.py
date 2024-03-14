05ddcaef14622c5d034581ecd40dac51b67c467e
Artem <oluwoartem@gmail.com>
2024-03-14 22:22:16 +0300

    removed command

--- common/commands.py
+++ common/commands.py
@@ -3,5 +3,4 @@ from aiogram.types import BotCommand
 private = [
     BotCommand(command="start", description="Запуск бота"),
     BotCommand(command="about", description="О боте"),
-    BotCommand(command="support", description="Связаться с поддержкой"),
 ]
